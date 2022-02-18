d88c64c0cc6d5feafa2bbea7a0f2734a31cb1a17
ron51301 <you@example.com>
2022-02-18 14:47:42 +0800

    add 123

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ import json
 import configparser
 import os
 from urllib import parse
-from datetime import datetime
+
+import time
 import pymysql
-from datetime import datetime
-    
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -34,6 +33,7 @@ HEADER = {
     'Authorization': F'Bearer {config.get("line-bot", "channel_access_token")}'
 }
 
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
@@ -48,66 +48,47 @@ def index():
         if events[0]["type"] == "message":
             if events[0]["message"]["type"] == "text":
                 text = events[0]["message"]["text"]
-                if text == "我的名字":
-                    payload["messages"] = [getNameEmojiMessage()]
-                elif text == "出去玩囉":
-                    payload["messages"] = [getPlayStickerMessage()]
-                elif text == "台北101":
-                    payload["messages"] = [getTaipei101ImageMessage(),
-                                           getTaipei101LocationMessage(),
-                                           getMRTVideoMessage()]
-                elif text == "台北101圖":
-                    payload["messages"] = [getTaipei101ImageMessage()]
-                elif text == "台北101影片":
-                    payload["messages"] = [getMRTVideoMessage()]
-                elif text == "quoda":
+
+                if text == "一般使用者":
                     payload["messages"] = [
-                            {
-                                "type": "text",
-                                "text": getTotalSentMessageCount()
-                            }
-                        ]
-                elif text == "今日確診人數":
-                    payload["messages"] = [
-                            {
-                                "type": "text",
-                                "text": getTodayCovid19Message()
-                            }
-                        ]
+                                            {
+                                            "type":"text",
+                                            "text":"Hello, user"
+                                            },
+                                            {
+                                                "type": "template",
+                                                "altText": "This is a buttons template",
+                                                "template": {
+                                                    "type": "buttons",
+                                                    "title": "Menu",
+                                                    "text": "Please select",
+                                                    "actions": [
+                                                        {
+                                                            "type": "message",
+                                                            "label": "cfi-102",
+                                                            "text": "cfi-102"
+                                                        },
+                                                        {
+                                                            "type": "message",
+                                                            "label": "cfi-103",
+                                                            "text": "cfi-103"
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                          ]
                 elif text == "打卡":
                     daka()
 
                 elif text == "打卡查詢":
                     select_where("WLOG", "EMPNO")
-                elif text == "主選單":
-                    payload["messages"] = [
-                            {
-                                "type": "template",
-                                "altText": "This is a buttons template",
-                                "template": {
-                                  "type": "buttons",
-                                  "title": "Menu",
-                                  "text": "Please select",
-                                  "actions": [
-                                      {
-                                        "type": "message",
-                                        "label": "我的名字",
-                                        "text": "我的名字"
-                                      },
-                                      {
-                                        "type": "message",
-                                        "label": "今日確診人數",
-                                        "text": "今日確診人數"
-                                      },
-                                      {
-                                        "type": "uri",
-                                        "label": "聯絡我",
-                                        "uri": f"tel:{my_phone}"
-                                      }
-                                  ]
-                              }
-                            }
-                        ]
+
+
+
+
+
+                    payload["messages"] = [getPlayStickerMessage()]
+
                 else:
                     payload["messages"] = [
                             {
@@ -116,84 +97,51 @@ def index():
                             }
                         ]
                 replyMessage(payload)
-            elif events[0]["message"]["type"] == "location":
-                title = events[0]["message"]["title"]
-                latitude = events[0]["message"]["latitude"]
-                longitude = events[0]["message"]["longitude"]
-                payload["messages"] = [getLocationConfirmMessage(title, latitude, longitude)]
-                replyMessage(payload)
-        elif events[0]["type"] == "postback":
-            if "params" in events[0]["postback"]:
-                reservedTime = events[0]["postback"]["params"]["datetime"].replace("T", " ")
-                payload["messages"] = [
-                        {
-                            "type": "text",
-                            "text": F"已完成預約於{reservedTime}的叫車服務"
-                        }
-                    ]
-                replyMessage(payload)
-            else:
-                data = json.loads(events[0]["postback"]["data"])
-                action = data["action"]
-                if action == "get_near":
-                    data["action"] = "get_detail"
-                    payload["messages"] = [getCarouselMessage(data)]
-                elif action == "get_detail":
-                    del data["action"]
-                    payload["messages"] = [getTaipei101ImageMessage(),
-                                           getTaipei101LocationMessage(),
-                                           getMRTVideoMessage(),
-                                           getCallCarMessage(data)]
-                replyMessage(payload)
 
     return 'OK'
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def pretty_echo(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
+def getNameEmojiMessage():
+    lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    productId = "5ac21a8c040ab15980c9b43f"
+    name = "Jason"
+    message = dict()
+    message["type"] = "text"
+    message["text"] = "".join("$" for r in range(len(name)))
+    emojis_list = list()
+    for i, nChar in enumerate(name):
+        emojis_list.append(
+            {
+                "index": i,
+                "productId": productId,
+                "emojiId": f"{lookUpStr.index(nChar) + 1 :03}"
+            }
         )
+    message["emojis"] = emojis_list
+    return message
 
 
-@app.route("/sendTextMessageToMe", methods=['POST'])
-def sendTextMessageToMe():
-    pushMessage({})
+def getPlayStickerMessage():
+    message = dict()
+    message["type"] = "sticker"
+    message["packageId"] = "6325"
+    message["stickerId"] = "10979904"
+    return message
+
+
+def replyMessage(payload):
+    response = requests.post("https://api.line.me/v2/bot/message/reply",headers=HEADER,data=json.dumps(payload))
     return 'OK'
 
 
-def daka():
-    connection = pymysql.connect(host="us-cdbr-east-05.cleardb.net",
-                                 user="b809ff374c792c",
-                                 password="bbc8de98",
-                                 database="heroku_9a97caadd884ab8")
+def pushMessage(payload):
+    response = requests.post("https://api.line.me/v2/bot/message/push",headers=HEADER,data=json.dumps(payload))
+    return 'OK'
 
-    cursor = connection.cursor()
-    create_date = time.strftime('%Y-%m-%d')  # 得到當前日期
-    create_time = time.strftime('%H:%M:%S')  # 得到當前時間
-    # 在mysql中，時間資料也是字串，故create_date和create_time還要有一組雙引號
-    sql = f"insert into wlog (EMPNO , CREATE_DATE, CREATE_TIME) values ('{168}', '{create_date}', '{create_time}')"
-    cursor.execute(sql)
 
-    connection.commit()
-    cursor.close()
-    connection.close()
+def getTotalSentMessageCount():
+    response = requests.get("https://api.line.me/v2/bot/message/quota/consumption",headers=HEADER)
+    return response.json()["totalUsage"]
 
 def select_where(WLOG, EMPNO):
     connection = pymysql.connect(host="us-cdbr-east-05.cleardb.net",
@@ -216,233 +164,22 @@ def select_where(WLOG, EMPNO):
             print(row)
 select_where("WLOG", 100)
 
+def daka():
+    connection = pymysql.connect(host="us-cdbr-east-05.cleardb.net",
+                                 user="b809ff374c792c",
+                                 password="bbc8de98",
+                                 database="heroku_9a97caadd884ab8")
 
-def getNameEmojiMessage():
-    lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    productId = "5ac21a8c040ab15980c9b43f"
-    name = "Ron"
-    message = dict()
-    message["type"] = "text"
-    message["text"] = "".join("$" for r in range(len(name)))
-    emojis_list = list()
-    for i, nChar in enumerate(name):
-        emojis_list.append(
-            {
-              "index": i,
-              "productId": productId,
-              "emojiId": f"{lookUpStr.index(nChar) + 1 :03}"
-            }
-        )
-    message["emojis"] = emojis_list
-    return message
+    cursor = connection.cursor()
+    create_date = time.strftime('%Y-%m-%d')  # 得到當前日期
+    create_time = time.strftime('%H:%M:%S')  # 得到當前時間
+    # 在mysql中，時間資料也是字串，故create_date和create_time還要有一組雙引號
+    sql = f"insert into wlog (EMPNO , CREATE_DATE, CREATE_TIME) values ('{168}', '{create_date}', '{create_time}')"
+    cursor.execute(sql)
 
-
-def getCarouselMessage(data):
-    message = {
-      "type": "template",
-      "altText": "this is a image carousel template",
-      "template": {
-          "type": "image_carousel",
-          "columns": [
-              {
-                "imageUrl": F"{end_point}/static/taipei_101.jpeg",
-                "action": {
-                  "type": "postback",
-                  "label": "台北101",
-                  "data": json.dumps(data)
-                }
-              },
-              {
-                "imageUrl": F"{end_point}/static/taipei_1.jpeg",
-                "action": {
-                  "type": "postback",
-                  "label": "台北101",
-                  "data": json.dumps(data)
-                }
-              }
-          ]
-          }
-        }
-    return message
-
-def getLocationConfirmMessage(title, latitude, longitude):
-    data = {'title': title, 'latitude': latitude, 'longitude': longitude,
-            'action': 'get_near'}
-    message = {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text": f"確認是否搜尋 {title} 附近地點？",
-          "actions": [
-              {
-                 "type": "postback",
-               "label": "是",
-               "data": json.dumps(data),
-               },
-              {
-                "type": "message",
-                "label": "否",
-                "text": "否"
-              }
-          ]
-      }
-    }
-    return message
-
-def getCallCarMessage(data):
-    message = {
-      "type": "template",
-      "altText": "this is a template",
-      "template": {
-          "type": "buttons",
-          "text": f"請選擇至 {data['title']} 預約叫車時間",
-          "actions": [
-              {
-               "type": "datetimepicker",
-               "label": "預約",
-               "data": json.dumps(data),
-               "mode": "datetime"
-               }
-          ]
-      }
-    }
-    return message
-
-
-def getPlayStickerMessage():
-    message = dict()
-    message["type"] = "sticker"
-    message["packageId"] = "446"
-    message["stickerId"] = "1988"
-    return message
-
-
-def getTaipei101LocationMessage():
-    message = dict()
-    message["type"] = "location"
-    message["title"] = "台北101"
-    message["address"] = "110台北市信義區信義路五段7號"
-    message["latitude"] = "25.034056468449304"
-    message["longitude"] = "121.56466736984362"
-    return message
-
-
-def getMRTVideoMessage():
-    message = dict()
-    message["type"] = "video"
-    message["originalContentUrl"] = F"{end_point}/static/taipei_101_video.mp4"
-    message["previewImageUrl"] = F"{end_point}/static/taipei_101.jpeg"
-    return message
-
-
-def getMRTSoundMessage():
-    message = dict()
-    message["type"] = "audio"
-    message["originalContentUrl"] = F"{end_point}/static/mrt_sound.m4a"
-    import audioread
-    with audioread.audio_open('static/mrt_sound.m4a') as f:
-        # totalsec contains the length in float
-        totalsec = f.duration
-    message["duration"] = totalsec * 1000
-    return message
-
-
-def getTaipei101ImageMessage(originalContentUrl=F"{end_point}/static/taipei_101.jpeg"):
-    return getImageMessage(originalContentUrl)
-
-
-def getImageMessage(originalContentUrl):
-    message = dict()
-    message["type"] = "image"
-    message["originalContentUrl"] = originalContentUrl
-    message["previewImageUrl"] = originalContentUrl
-    return message
-
-
-def replyMessage(payload):
-    response = requests.post("https://api.line.me/v2/bot/message/reply",headers=HEADER,data=json.dumps(payload))
-    return 'OK'
-
-
-def pushMessage(payload):
-    response = requests.post("https://api.line.me/v2/bot/message/push",headers=HEADER,data=json.dumps(payload))
-    return 'OK'
-
-
-def getTotalSentMessageCount():
-    response = requests.get("https://api.line.me/v2/bot/message/quota/consumption",headers=HEADER)
-    return response.json()["totalUsage"]
-
-
-def getTodayCovid19Message():
-    response = requests.get("https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4001&limited=TWN")
-    date = response.json()[0]["a04"]
-    total_count = response.json()[0]["a05"]
-    count = response.json()[0]["a06"]
-    return F"日期：{date}, 人數：{count}, 確診總人數：{total_count}"
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-@app.route('/upload_file', methods=['POST'])
-def upload_file():
-    payload = dict()
-    if request.method == 'POST':
-        file = request.files['file']
-        print("json:", request.json)
-        form = request.form
-        age = form['age']
-        gender = ("男" if form['gender'] == "M" else "女") + "性"
-        if file:
-            filename = file.filename
-            img_path = os.path.join(UPLOAD_FOLDER, filename)
-            file.save(img_path)
-            print(img_path)
-            payload["to"] = my_line_id
-            payload["messages"] = [getImageMessage(F"{end_point}/{img_path}"),
-                {
-                    "type": "text",
-                    "text": F"年紀：{age}\n性別：{gender}"
-                }
-            ]
-            pushMessage(payload)
-    return 'OK'
-
-
-@app.route('/line_login', methods=['GET'])
-def line_login():
-    if request.method == 'GET':
-        code = request.args.get("code", None)
-        state = request.args.get("state", None)
-
-        if code and state:
-            HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
-            url = "https://api.line.me/oauth2/v2.1/token"
-            FormData = {"grant_type": 'authorization_code', "code": code, "redirect_uri": F"{end_point}/line_login", "client_id": line_login_id, "client_secret":line_login_secret}
-            data = parse.urlencode(FormData)
-            content = requests.post(url=url, headers=HEADERS, data=data).text
-            content = json.loads(content)
-            url = "https://api.line.me/v2/profile"
-            HEADERS = {'Authorization': content["token_type"]+" "+content["access_token"]}
-            content = requests.get(url=url, headers=HEADERS).text
-            content = json.loads(content)
-            name = content["displayName"]
-            userID = content["userId"]
-            pictureURL = content["pictureUrl"]
-            statusMessage = content["statusMessage"]
-            print(content)
-            return render_template('profile.html', name=name, pictureURL=
-                                   pictureURL, userID=userID, statusMessage=
-                                   statusMessage)
-        else:
-            return render_template('login.html', client_id=line_login_id,
-                                   end_point=end_point)
-
-
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 if __name__ == "__main__":
     app.debug = True
